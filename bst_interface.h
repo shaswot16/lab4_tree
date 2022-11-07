0fe208f173e6ef96a84ec12a92c01d759bc36cb4
1shan-pixel <ispanta03@gmail.com>
2022-11-07 14:29:32 +0545

    interface class pushed

---
+++ bst_interface.h
@@ -0,0 +1,16 @@
+
+#include<iostream>
+
+#ifndef bst_interface_h
+#define bst_interface_h 
+
+
+class AbstractBinarySearchTree{
+    virtual void addBST(int value, int target) =0; 
+    virtual bool isEmpty() =0; 
+    virtual void deleteBST(int value , int target)=0; 
+    virtual bool searchBST(int target)= 0; 
+
+};
+
+#endif // !bst_interface_h
